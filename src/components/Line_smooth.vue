78a0308523d250746d2ac6a1bcbdd8ff11b765b1
Richardy <136699656@qq.com>
2023-03-31 22:49:12 +0800

    add:折线图

---
+++ src/components/Line_smooth.vue
@@ -0,0 +1,130 @@
+<script>
+import { getCurrentInstance, onMounted , reactive, toRefs, onUnmounted, watch} from 'vue';
+
+export default{
+    name:'radar_3',
+    props:{
+        chartName:String,
+        x_Axis:Array,
+        chartData:Array,
+    },
+    setup(props){
+        const dataMap = reactive({
+            chartName: props.chartName,
+            x_Axis: props.x_Axis,
+            chartData: props.chartData,
+            option: {},
+            myChart: null,
+        })
+        const chartPrepare = () => {
+            console.log(dataMap.chartData);
+            // let internalInstance = getCurrentInstance();
+            // let echarts = internalInstance.appContext.config.globalProperties.$echarts;
+            // //图表的id父级传过来 防止一个页面多个图不绘制，还能防止vue3.x 还有打包之后初始化不绘制的问题  原理我也不知道  有大佬告知一下最好  
+            // dataMap.myChart = echarts.init(document.getElementById(dataMap.chartName));
+            // 绘制图表需要的数据
+            dataMap.option = {
+                // color: ['rgba(64, 119, 255, 0.6)', 'rgba(236, 112, 192, 0.6)'],
+                title: {
+                    text: '细目折线图'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    show: true,
+                },
+                legend: {
+                    data: ['个人综合素养水平', '平均水平']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                
+                toolbox: {
+                    feature: {
+                    saveAsImage: {}
+                    }
+                },
+                
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                    data: dataMap.x_Axis,
+                    smooth: true
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: '个人综合素养水平',
+                        data: dataMap.chartData[0],
+                        type: 'line',
+                        smooth: true,
+                        
+                    },
+                    {
+                        name: '平均水平',
+                        data: dataMap.chartData[1],
+                        type: 'line',
+                        smooth: true
+                    },
+                ]
+            }
+        }
+        // 绘制图表
+        const chartOpen = () => {
+            // 这里是tooltip有bug，不能直接把echarts实例赋值给reactive对象即dataMap.mychart，所以额外在这里定义一个chart，然后再赋值给reactive对象 
+            let internalInstance = getCurrentInstance();
+            let echarts = internalInstance.appContext.config.globalProperties.$echarts;
+            const chart = echarts.init(document.getElementById(dataMap.chartName))
+            chart.setOption(dataMap.option, true);
+            dataMap.myChart = chart
+
+            // window.onresize = function() { // 自适应大小
+            // 这个自适应大小我反正疯狂报错,应该是resize() 是一个需要配置的还是什么的玩意 搞不明白暂时不用了
+            // 	dataMap.myChart.resize();
+            // };
+        }
+
+        onMounted(() => {
+            // 基础数据
+            chartPrepare()
+            // 绘制图表
+            chartOpen()
+        });
+
+        watch(() => props.chartData,
+            (count, prevCount) => {
+                // 监听数据变了  就重新绘制一遍  也就是大佬们说的  "切记，数据变化后需要再次调init方法刷线图表"
+                // 绘制图表
+                chartOpen()
+                console.log(count, prevCount);
+            }, {
+                deep: true
+            }
+        )
+
+        onUnmounted(() => {
+            //销毁
+            echarts.dispose(dataMap.myChart);
+            dataMap.myChart = null;
+        })
+
+        return {
+            ...toRefs(dataMap),
+        }
+    }
+}
+
+
+
+
+
+</script>
+<template>
+    <div ref="line_smooth" :id="chartName" style="width: 100%; height: 400px"></div>
+</template>
--- src/view/SingleProfile.vue
+++ src/view/SingleProfile.vue
@@ -4,6 +4,7 @@ import Bar from '../components/Bar.vue'
 import test from '../components/test.vue'
 import radar_3 from '../components/Radar_3.vue'
 import radar_10 from '../components/Radar_10.vue'
+import line_smooth from '../components/Line_smooth.vue'
 import {  ref } from 'vue'
 
 const data_3 = [
@@ -12,8 +13,11 @@ const data_3 = [
 const data_10 = [
   [80,70,60,72,88,75,63,92,90,79],[70,60,90,70,80,90,75,81,84,73]
 ]
-  const chartData_3 = ref(data_3)
-  const chartData_10 = ref(data_10)
+const data_ximu = ['细目1','细目2','细目3','细目4','细目5','细目6','细目7','细目8','细目9','细目10']
+const chartData_3 = ref(data_3)
+const chartData_10 = ref(data_10)
+const chartData_line = ref(data_10)
+const x_Axis_ximu = ref(data_ximu)
 </script>
 <template>
   <div>
@@ -21,5 +25,8 @@ const data_10 = [
   </div>
   <div>
     <radar_10 :chartData = "chartData_10" :chartName="'radar_10'"></radar_10>
+  </div>
+  <div>
+    <line_smooth :x_Axis="x_Axis_ximu"  :chartData = "chartData_line" :chartName="'line_smooth'"></line_smooth>
   </div>
 </template>
